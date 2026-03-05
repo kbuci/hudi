b6f9c00d44e1681b272506c402077f28b833eab4
Krishen Bhan <bkrishen@uber.com>
2026-03-04 19:23:02 -0800

    replace add

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
@@ -679,9 +679,9 @@ public class HoodieWriteConfig extends HoodieConfig {
       .key("hoodie.clustering.block_for_pending_ingestion")
       .defaultValue(false)
       .markAdvanced()
-      .withDocumentation("When enabled, proactively prevents clustering from committing if there are any ongoing ingestion "
+      .withDocumentation("(Only applicable to PreferWriterConflictResolutionStrategy) When enabled, proactively prevents clusteringfrom committing if there are any ongoing ingestion "
           + "writes that have not transitioned from requested to inflight yet and have an active heartbeat, "
-          + "since ingestion may be targeting the same files and should have precedence.");
+          + "since ingestion may be targeting the same files and should have precedence (for that conflict resolution strategy).");
 
   public static final ConfigProperty<String> WRITE_CONCURRENCY_MODE = ConfigProperty
       .key("hoodie.write.concurrency.mode")
