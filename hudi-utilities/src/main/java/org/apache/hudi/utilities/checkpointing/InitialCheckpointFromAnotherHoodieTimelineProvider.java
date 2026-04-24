2b3416431b6ee02db0e1650d17b607172d02a813
Krishen Bhan <bkrishen@uber.com>
2026-04-23 19:24:05 -0700

    feat(common): When inferring checkpoint/schema from timeline, check non-ingestion write commits (in case they have metadata rolled-over

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/checkpointing/InitialCheckpointFromAnotherHoodieTimelineProvider.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/checkpointing/InitialCheckpointFromAnotherHoodieTimelineProvider.java
@@ -22,6 +22,7 @@ import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.model.HoodieCommitMetadata;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
 import org.apache.hudi.common.table.checkpoint.CheckpointUtils;
+import org.apache.hudi.common.util.StringUtils;
 import org.apache.hudi.exception.HoodieException;
 import org.apache.hudi.exception.HoodieIOException;
 import org.apache.hudi.hadoop.fs.HadoopFSUtils;
@@ -29,7 +30,6 @@ import org.apache.hudi.hadoop.fs.HadoopFSUtils;
 import org.apache.hadoop.conf.Configuration;
 
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * This is used to set a checkpoint from latest commit of another (mirror) hudi dataset.
@@ -68,7 +68,7 @@ public class InitialCheckpointFromAnotherHoodieTimelineProvider extends InitialC
           } catch (IOException e) {
             throw new HoodieIOException("Failed to read commit metadata for instant " + instant.requestedTime(), e);
           }
-        }).filter(Objects::nonNull).findFirst()
+        }).filter(key -> !StringUtils.isNullOrEmpty(key)).findFirst()
         .orElseThrow(() -> new HoodieException("Unable to find checkpoint in source table at: "
             + path + ". This table may not have been created with checkpoint tracking enabled."));
   }
